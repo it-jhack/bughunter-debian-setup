7cf716c794692206fe9a5ec77eb69cc0869ee6e8
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-20 19:01:56 -0300

    add pip, jq, shosubgo

--- debian10.sh
+++ debian10.sh
@@ -2,24 +2,32 @@
 
 # Download Links/vars
 # url example = https://dl.google.com/go/go1.16.2.linux-amd64.tar.gz
+
+# Golang download
 echo -n "Enter Golang (for Debian/Linux) download URL: "
 read go_url
 go_filename=${go_url##*/} # strip last part of $go_url
+
+# Shodan.io API to create environment var
+echo -n "Enter your shodan.io API key (free registration): "
+read shodan_api
 
 ####################################################
 #PACKAGE MANAGERS
 
 # apt && apt-get update/upgrade
-apt update && apt upgrade
-apt-get update && apt-get upgrade
+apt update && apt upgrade -y
+apt-get update && apt-get upgrade -y
 
 # install snap
 apt install snapd -y
 
 # apt packages
 apt install git -y
+apt install python3-pip -y
 
 # apt-get packages
+apt-get install jq -y
 apt-get install tree -y
 
 ####################################################
@@ -54,3 +62,17 @@ go build
 mv nuclei /usr/local/bin/
 nuclei -version
 nuclei -update-templates
+
+# shosubgo (shodan subdomain enumerator)
+cd ~/go/pkg/mod/github.com/
+git clone https://github.com/pownx/shosubgo.git #!TODO script shosubgo installation/run
+echo "" >> ~/.profile
+echo "export SHODAN_API=$shodan_api" >> ~/.bashrc
+source ~/.profile
+touch ~/.bash_aliases
+echo echo "" >> ~/.bash_aliases
+echo "alias shosubgo='go run ~/go/pkg/mod/github.com/shosubgo/main.go'" >> ~/.bash_aliases
+source ~/.bash_aliases
+
+# pip3 packages
+pip3 install requests
