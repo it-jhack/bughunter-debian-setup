4a830d2027b5af2728dcae834e9c3f5b95bcf972
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-21 11:48:35 -0300

    add hakrawler, assetfinder

--- debian10.sh
+++ debian10.sh
@@ -12,6 +12,14 @@ go_filename=${go_url##*/} # strip last part of $go_url
 echo -n "Enter your shodan.io API key (free registration): "
 read shodan_api
 
+# VirusTotal API key
+echo -n "Enter your virustotal.com API key (free registration): "
+read virustotal_api
+
+# SPYSE API key [Optional]
+echo -n "[Optional] Enter your spyse.com API key (used on 'assetfinder'): "
+read spyse_api
+
 ####################################################
 #PACKAGE MANAGERS
 
@@ -70,11 +78,35 @@ cd shosubgo
 curl -LJO https://github.com/pownx/shosubgo/releases/download/1.1/shosubgo_linux_1_1
 chmod +x shosubgo_linux_1_1
 echo "" >> ~/.profile
-echo "export SHODAN_API=$shodan_api" >> ~/.bashrc
+echo "export SHODAN_API=$shodan_api" >> ~/.profile
 source ~/.profile
 echo "" >> ~/.bashrc
 echo "alias shosubgo='\$HOME/go/pkg/mod/github.com/shosubgo/shosubgo_linux_1_1'" >> ~/.bashrc
 source ~/.bashrc
+
+# hakrawler by hakluke
+go get github.com/hakluke/hakrawler
+echo "" >> ~/.bashrc
+echo "alias hakrawler='\$HOME/go/bin/hakrawler'" >> ~/.bashrc
+source ~/.bashrc
+
+# assetfinder by tomnomnom
+go get -u github.com/tomnomnom/assetfinder
+echo "" >> ~/.profile
+echo "export VT_API_KEY=\"$virustotal_api\"" >> ~/.profile
+if [ -z "$spyse_api" ] # if spyse_api is empty
+then
+      echo "User opted not to provide spyse.com API key"
+else
+      echo "export SPYSE_API_TOKEN=\"$spyse_api\"" >> ~/.profile
+fi
+source ~/.profile
+echo "" >> ~/.bashrc
+echo "alias assetfinder=\"\$HOME/go/bin/assetfinder\"" >> ~/.bashrc
+source ~/.bashrc
+
+####################################################
+# Python
 
 # pip3 packages
 pip3 install requests
